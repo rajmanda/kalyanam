6da02a1034b3a8ba9da88e3974e3e6aac9f04dd9
Raj Manda <raj.manda@gmail.com>
2024-09-15 20:42:44 -0400

    Adding login and logout functionality

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,15 +3,40 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatToolbarModule,MatButtonModule, RouterLink],
+  imports: [MatToolbarModule, MatIconModule, MatToolbarModule,MatButtonModule, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  userProfilex: any;
+  loggedin: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userProfilex = this.authService.getUserProfile();
+
+    if (this.userProfilex && Object.keys(this.userProfilex).length > 0) {
+      // userProfilex is an object and has some properties
+      console.log('userProfilex has properties:', this.userProfilex);
+      this.loggedin = true ;
+    } else {
+      // userProfilex is empty, null, or undefined
+      console.log('userProfilex is empty or undefined');
+    }
+
+  }
+
+  handleSignOut() {
+    this.authService.signOut();
+    this.loggedin = false ;
+  }
 }
