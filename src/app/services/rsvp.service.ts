69a9f9f70bd3fc31c56ce1c5ee1371c62c1ee0d8
rajmanda <raj.manda@gmail.com>
2024-10-04 13:36:49 -0400

    Update rsvp.service.ts to use environment variable for the rsvp service url

--- src/app/services/rsvp.service.ts
+++ src/app/services/rsvp.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RsvpDTO} from '../models/rsvpDTO';
-
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RsvpService {
 
-  private baseUrl = 'http://localhost:8080/rsvp';
+  private rsvpApiUrl = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.rsvpBackendBaseUrl  = environment.rsvpApiUrl;
+    console.log(environment.production);
+  }
 
   saveRsvp(rsvpEvent: RsvpDTO): Observable<RsvpDTO> {
     console.log(`RsvpDTO being sent to be saved to backend - ${JSON.stringify(rsvpEvent, null, 2)}`)
-    return this.http.post<RsvpDTO>(`${this.baseUrl}/saversvp`, rsvpEvent);
+    return this.http.post<RsvpDTO>(`${this.rsvpApiUrl}/saversvp`, rsvpEvent);
   }
 }
