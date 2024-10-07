03d1323da2cec42a7c929456c7b57fb10f31c1df
rajmanda <raj.manda@gmail.com>
2024-10-07 17:34:42 +0000

    full DNS name format when calling the service

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    // rsvpApiUrl: 'http://localhost:8080/rsvp'
-    rsvpApiUrl: 'http://rsvpbackend:8090'
+    rsvpApiUrl: 'http://localhost:8080/rsvp'
+    //rsvpApiUrl: 'http://rsvpbackend:8090'
+    //rsvpApiUrl: 'http://rsvpbackend.kalyanam.svc.cluster.local:8090'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     // rsvpApiUrl: 'http://rajmanda-dev.com/rsvp'
-    rsvpApiUrl:'http://rsvpbackend:8090'
+    //rsvpApiUrl:'http://rsvpbackend:8090'
+    rsvpApiUrl: 'http://rsvpbackend.kalyanam.svc.cluster.local:8090/rsvp'
   };
