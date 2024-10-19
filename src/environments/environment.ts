0db51d4a4102c16aa8c209b4df81d9b1b52ec890
Raj Manda <raj.manda@gmail.com>
2024-10-19 13:57:46 -0400

    Enabling https for running in kubernetes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-     rsvpApiUrl: 'http://rajmanda-dev.com/rsvp'
+     rsvpApiUrl: 'https://rajmanda-dev.com/rsvp'
     //rsvpApiUrl:'http://rsvpbackend:8090/rsvp'
     //eurekaServerUrl: 'http://my-eureka.default.svc.cluster.local:8761/rsvp',
     //rsvpApiUrl: 'http://my-eureka.default.svc.cluster.local:8761/eureka/instances/rsvpbackend/rsvp',
