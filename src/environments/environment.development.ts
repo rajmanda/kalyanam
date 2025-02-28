e28a363ad95e8ee5300d490a154e3494a0011f10
Raj Manda <raj.manda@gmail.com>
2025-02-28 13:48:49 -0500

    Adding Google Client ID to environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    googleClientId: '175415323680-4t3u58105ihj3sk9di0er3kv70cf3arh.apps.googleusercontent.com',
     production: false,
     rsvpApiUrl: 'http://localhost:8080/rsvp',
     galaEventsApiUrl: 'http://localhost:8080/gala-event',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
+    googleClientId: '175415323680-4t3u58105ihj3sk9di0er3kv70cf3arh.apps.googleusercontent.com',
     production: true,
-     rsvpApiUrl: 'https://rajmanda-dev.com/rsvp',
-     galaEventsApiUrl: 'https://rajmanda-dev.com/gala-event',
+    rsvpApiUrl: 'https://rajmanda-dev.com/rsvp',
+    galaEventsApiUrl: 'https://rajmanda-dev.com/gala-event',
 
     //rsvpApiUrl:'http://rsvpbackend:8090/rsvp'
     //eurekaServerUrl: 'http://my-eureka.default.svc.cluster.local:8761/rsvp',
