e68c0bc8c26b0e8120749ab652e2771f4ce519b3
Deepak Kumar Tiwari <deepakkumar2025@gmail.com>
2025-02-28 19:32:20 +0530

    deepak

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    rsvpApiUrl: 'http://localhost:8080/rsvp',
-    galaEventsApiUrl: 'http://localhost:8080/gala-event',
+    rsvpApiUrl: 'https://rajmanda-dev.com/rsvp',
+    galaEventsApiUrl: 'https://rajmanda-dev.com/gala-event',
     //rsvpApiUrl: 'http://rsvpbackend:8090/rsvp'
     //rsvpApiUrl: 'http://rsvpbackend.kalyanam.svc.cluster.local:8090/rsvp'
     //rsvpApiUrl: 'http://RSVPBACKEND/rsvp'
