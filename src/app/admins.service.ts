2896a251c2cae0a014eb9af0b0a6dff5443a0ba3
rajmanda <raj.manda@gmail.com>
2025-03-03 22:16:46 -0500

    Feature/for guest rsvp (#8)
    * Initial Changes for Guest RSVP
    
    * Update Docker image to latest version - 168
    
    * Update attendees-dialog.component.html - to show for Guest
    
    * Update Docker image to latest version - 170
    
    * Update rsvpDTO.ts - Adding missing field - 'forGuest'
    
    * Update Docker image to latest version - 171
    
    * Update attendees-dialog.component.html
    
    * Update attendees-dialog.component.ts
    
    * Update Docker image to latest version - 173
    
    * Update kalyanam-deployment.yaml
    
    * Update Docker image to latest version - 174
    
    * Update kalyanam-deployment.yaml
    
    * Update Docker image to latest version - 175
    
    * Adding functionality to add guests and also show totals on attendees
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ src/app/admins.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AdminsDTO } from './models/admins';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminsService {
+
+    private adminsApiUrl = '';
+    data: AdminsDTO[]  = [];
+
+    constructor(private http: HttpClient) {
+      this.adminsApiUrl  = environment.adminsApiUrl;
+      console.log(environment.adminsApiUrl);
+    }
+
+    getAdmins(): Observable<AdminsDTO[]> {
+      return this.http.get<AdminsDTO[]>(`${this.adminsApiUrl}/alladmins`);
+    }
+
+    isAdmin(email: string): Observable<boolean> {
+      return this.getAdmins().pipe(
+        map(admins => {
+          // Check if any admin has the matching email
+          const isAdmin = admins.some(admin => {
+            return admin.email === email;
+          });
+          return isAdmin; // Return true or false
+        })
+      );
+    }
+}
