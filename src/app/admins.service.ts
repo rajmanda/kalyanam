003bb179993767cebe02a739d010a9a0ac11bbc7
rajmanda <raj.manda@gmail.com>
2025-03-06 13:54:21 -0500

    Deepak dev (#13)
    * changes in the admin service file
    
    * Update Docker image to latest version - 196
    
    * Update Docker image to latest version - 197
    
    * changes done in the footer
    
    * chagnes done in the footer css
    
    * Update Docker image to latest version - 208
    
    * images updated and descriptions added
    
    * changes in the css and image update
    
    * Changes in the css file
    
    * Update Docker image to latest version - 212
    
    * changes done in the create gala event page
    
    * Update Docker image to latest version - 214
    
    * update done on the formpage and homepage
    
    * Update Docker image to latest version - 215
    
    ---------
    
    Co-authored-by: Deepak Kumar Tiwari <deepakkumar2025@gmail.com>
    Co-authored-by: GitHub Action <action@github.com>

--- src/app/admins.service.ts
+++ src/app/admins.service.ts
@@ -1,42 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc'; // Import OAuthService
 import { AdminsDTO } from './models/admins';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminsService {
+  private adminsApiUrl = '';
+  data: AdminsDTO[] = [];
 
-    private adminsApiUrl = '';
-    data: AdminsDTO[]  = [];
+  constructor(private http: HttpClient, private oauthService: OAuthService) {
+    this.adminsApiUrl = environment.adminsApiUrl;
+    console.log(environment.adminsApiUrl);
+  }
 
-    constructor(private http: HttpClient) {
-      this.adminsApiUrl  = environment.adminsApiUrl;
-      console.log(environment.adminsApiUrl);
-    }
+  getAdmins(): Observable<AdminsDTO[]> {
+    const token = this.oauthService.getAccessToken(); // Now properly injected
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.get<AdminsDTO[]>(`${this.adminsApiUrl}/alladmins`, {
+      headers,
+    });
+  }
 
-    getAdmins(): Observable<AdminsDTO[]> {
-      // Get the access token from the OAuthService
-      const token = this.oauthService.getAccessToken();
-      // Set the Authorization header with the Bearer token
-      const headers = new HttpHeaders({
-        'Authorization': `Bearer ${token}`
-      });
-      return this.http.get<AdminsDTO[]>(`${this.adminsApiUrl}/alladmins`, { headers });
-    }
-
-    isAdmin(email: string): Observable<boolean> {
-      return this.getAdmins().pipe(
-        map(admins => {
-          // Check if any admin has the matching email
-          const isAdmin = admins.some(admin => {
-            return admin.email === email;
-          });
-          return isAdmin; // Return true or false
-        })
-      );
-    }
+  isAdmin(email: string): Observable<boolean> {
+    return this.getAdmins().pipe(
+      map((admins) => {
+        return admins.some((admin) => admin.email === email);
+      })
+    );
+  }
 }
