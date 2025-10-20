13e3a70d4132dd461975ca8fc1c57ef6c147489e
Raj Manda <raj.manda@gmail.com>
2025-10-20 19:04:27 -0400

    configuring the environments to point to cloudrun url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,15 @@ export const environment = {
     redirectUri: window.location.origin + '/callback',
   },
   adminEmails: ['raj.manda@gmail.com', 'msvram@gmail.com'], // Add admin
-  apiBaseUrl: window.location.origin, // Base URL for APIs
-  rsvpApiUrl: window.location.origin + '/rsvp', // Full URL for RSVP API
-  galaEventsApiUrl: window.location.origin + '/gala-event', // Full URL for Gala Events API
-  adminsApiUrl: window.location.origin + '/admins',
-  fileUploadApiUrl: window.location.origin + '/upload',
+  // apiBaseUrl: window.location.origin, // Base URL for APIs
+  // rsvpApiUrl: window.location.origin + '/rsvp', // Full URL for RSVP API
+  // galaEventsApiUrl: window.location.origin + '/gala-event', // Full URL for Gala Events API
+  // adminsApiUrl: window.location.origin + '/admins',
+  // fileUploadApiUrl: window.location.origin + '/upload',
+
+  apiBaseUrl: 'https://rsvp-backend-175415323680.us-central1.run.app', // Base URL for APIs
+  rsvpApiUrl: 'https://rsvp-backend-175415323680.us-central1.run.app/rsvp', // Full URL for RSVP API
+  galaEventsApiUrl: 'https://rsvp-backend-175415323680.us-central1.run.app/gala-event', // Full URL for Gala Events API
+  adminsApiUrl: 'https://rsvp-backend-175415323680.us-central1.run.app/admins',
+  fileUploadApiUrl: 'https://rsvp-backend-175415323680.us-central1.run.app/upload',
 };
